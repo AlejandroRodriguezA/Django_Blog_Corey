9826c6b2274a4f7a10784809d5592987d1546d7f
Alejandro <rodriguezalvarez@gmail.com>
2021-01-26 11:33:31 +0100

    Fixed error en static_root

--- Landing_AJ/settings.py
+++ Landing_AJ/settings.py
@@ -154,5 +154,4 @@ AWS_DEFAULT_ACL='public-read'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 AWS_S3_REGION_NAME = "eu-west-2"
