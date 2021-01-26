27026abdf93fa9ac50eade1fdc383b8f062996ab
Alejandro <rodriguezalvarez@gmail.com>
2021-01-26 21:40:27 +0100

    Settings, import django_heroku y locals

--- Landing_AJ/settings.py
+++ Landing_AJ/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -175,3 +176,6 @@ AWS_DEFAULT_ACL='public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = "eu-west-2"
+
+
+django_heroku.settings(locals())
